0476328f717a450f8e060f08492883c44f8f8b83
shyjana1 <75596257+shyjana1@users.noreply.github.com>
2021-07-17 13:30:40 +0300

    Add files via upload

---
+++ ghostmoves.js
@@ -0,0 +1,26 @@
+import {
+  DIRECTIONS,
+  OBJECT_TYPE
+} from './setup';
+
+export function randomMovement(position, direction, objectExist) {
+  let dir = direction;
+  let nextMovePos = position + dir.movement;
+  const keys = Object.keys(DIRECTIONS);
+
+  while (
+    objectExist(nextMovePos, OBJECT_TYPE.WALL) || objectExist(nextMovePos, OBJECT_TYPE.GHOST)
+  ) {
+    // Get a random key numbre from that array
+    const key = keys[Math.floor(Math.random() * keys.length)];
+    // Set the new direction
+    dir = DIRECTIONS[key];
+    // Set the next move
+    nextMovePos = position + dir.movement;
+  }
+
+  return {
+    nextMovePos,
+    direction: dir
+  };
+}
---
+++ setup.js
@@ -0,0 +1,88 @@
+export const GRID_SIZE = 20;
+export const CELL_SIZE = 20;
+export const DIRECTIONS = {
+  // keys of the "joystick", used in the Pacman component
+  ArrowLeft: {
+    code: 37,
+    movement: -1,
+    rotation: 180
+  },
+  ArrowUp: {
+    code: 38,
+    movement: -GRID_SIZE,
+    rotation: 270
+  },
+  ArrowRight: {
+    code: 39,
+    movement: 1,
+    rotation: 0
+  },
+  ArrowDown: {
+    code: 40,
+    movement: GRID_SIZE,
+    rotation: 90
+  }
+};
+
+export const OBJECT_TYPE = {
+  // map of each characters and elements
+  BLANK: 'blank',
+  WALL: 'wall',
+  DOT: 'dot', //point
+  BLINKY: 'blinky', //ghost
+  PINKY: 'pinky', // ghost
+  INKY: 'inky', // ghost
+  CLYDE: 'clyde', //ghost
+  PILL: 'pill',
+  PACMAN: 'pacman',
+  GHOST: 'ghost', // general ghost
+  SCARED: 'scared', //pill eaten mode
+  GHOSTLAIR: 'lair' // ghosts starting point
+};
+
+export const CLASS_LIST = [
+  OBJECT_TYPE.BLANK,
+  OBJECT_TYPE.WALL,
+  OBJECT_TYPE.DOT,
+  OBJECT_TYPE.BLINKY,
+  OBJECT_TYPE.PINKY,
+  OBJECT_TYPE.INKY,
+  OBJECT_TYPE.CLYDE,
+  OBJECT_TYPE.PILL,
+  OBJECT_TYPE.PACMAN,
+  OBJECT_TYPE.GHOSTLAIR
+];
+
+
+export const LEVEL = [
+  // mapping of the game board:
+  // 0 = blank
+  // 1 = wall 
+  // 2 = dot.
+  // 7 = pill.
+  // 9 = ghostlair.
+
+  1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+  1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1,
+  1, 2, 1, 1, 2, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 2, 1, 1, 2, 1,
+  1, 7, 1, 1, 2, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 2, 1, 1, 7, 1,
+  1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1,
+  1, 2, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 2, 1,
+  1, 2, 2, 2, 2, 1, 2, 2, 2, 1, 1, 2, 2, 2, 1, 2, 2, 2, 2, 1,
+  1, 1, 1, 1, 2, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 2, 1, 1, 1, 1,
+  0, 0, 0, 1, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 1, 0, 0, 0,
+  0, 0, 0, 1, 2, 1, 2, 1, 9, 9, 9, 9, 1, 2, 1, 2, 1, 0, 0, 0,
+  1, 1, 1, 1, 2, 1, 2, 1, 9, 9, 9, 9, 1, 2, 1, 2, 1, 1, 1, 1,
+  1, 0, 0, 0, 2, 2, 2, 1, 9, 9, 9, 9, 1, 2, 2, 2, 0, 0, 0, 1,
+  1, 1, 1, 1, 2, 1, 2, 1, 9, 9, 9, 9, 1, 2, 1, 2, 1, 1, 1, 1,
+  0, 0, 0, 1, 2, 1, 2, 1, 1, 1, 1, 1, 1, 2, 1, 2, 1, 0, 0, 0,
+  0, 0, 0, 1, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 1, 0, 0, 0,
+  1, 1, 1, 1, 2, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 2, 1, 1, 1, 1,
+  1, 2, 2, 2, 2, 1, 2, 2, 2, 1, 1, 2, 2, 2, 1, 2, 2, 2, 2, 1,
+  1, 2, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 2, 1,
+  1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1,
+  1, 7, 1, 1, 2, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 2, 1, 1, 7, 1,
+  1, 2, 1, 1, 2, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 2, 1, 1, 2, 1,
+  1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1,
+  1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+];
